bcb66ea05efad3a19e3fc418fa1a2879ae7d5fc2
Koen Kooi <koen@dominion.thruhere.net>
2025-11-16 09:24:46 +0100

    MINI: enable Junction Deviation
    Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

--- include/marlin/Configuration_MINI.h
+++ include/marlin/Configuration_MINI.h
@@ -519,9 +519,10 @@
 //
 // Use Junction Deviation instead of traditional Jerk Limiting
 //
-#define CLASSIC_JERK
+// #define JUNCTION_DEVIATION
+// #define CLASSIC_JERK
 #if DISABLED(CLASSIC_JERK)
-    #define JUNCTION_DEVIATION_MM 0.01f // (mm) Distance from real junction edge
+    #define JUNCTION_DEVIATION_MM 0.0128f // (mm) Distance from real junction edge
     #define JD_SMALL_SEGMENT_HANDLING   // Handle small segments (< 1 mm) with large junction angles (> 135°) based on a local curvature estimate, instead of just the junction angle.
     //#define JD_DEBUG_OUTPUT           // Output junction_cos_theta and vmax_junction_sqr to serial
 #endif
