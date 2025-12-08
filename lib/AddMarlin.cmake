6edb4a0f8efe45d18bc7dc3cbcb0b4b1ad90cda2
espr14 <espr14@gmail.com>
2025-12-08 11:26:21 +0100

    lib/Marlin: Add M852 (skew correction)
    Koen: Removed MINI configuration change and menu items

--- lib/AddMarlin.cmake
+++ lib/AddMarlin.cmake
@@ -66,7 +66,8 @@ if(BOARD_IS_MASTER_BOARD)
             Marlin/Marlin/src/gcode/calibrate/G28.cpp
             Marlin/Marlin/src/gcode/calibrate/G80.cpp
             Marlin/Marlin/src/gcode/calibrate/M666.cpp
-            Marlin/Marlin/src/gcode/config/M200-M205.cpp
+            Marlin/Marlin/src/gcode/calibrate/M852.cpp
+	    Marlin/Marlin/src/gcode/config/M200-M205.cpp
             Marlin/Marlin/src/gcode/config/M217.cpp
             Marlin/Marlin/src/gcode/config/M218.cpp
             Marlin/Marlin/src/gcode/config/M220.cpp
--- lib/Marlin/Marlin/src/module/planner.h
+++ lib/Marlin/Marlin/src/module/planner.h
@@ -340,6 +340,8 @@ class Planner {
     static float max_printed_z;
 
     static skew_factor_t skew_factor;
+    // FIXME remove once not needed
+    static skew_factor_t get_skew_factor() {return skew_factor;}
 
     #if ENABLED(SD_ABORT_ON_ENDSTOP_HIT)
       static bool abort_on_endstop_hit;
